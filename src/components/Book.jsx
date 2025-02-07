e2c4ef5407ea3634ebddd1d174e351eb72468963
Vaishnavi187 <vaishnavisingh2005.college@gmail.com>
2025-02-07 14:44:16 +0530

    Update Book.jsx

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -11,7 +11,7 @@ const Book = () => {
   useEffect(()=>{
  const getbook=async()=>{
   try {
- const res=   await axios.get ("http://localhost:4000/book/") 
+ const res=   await axios.get ("https://backend-ybu6.onrender.com/book/") 
 
  setbook(res.data.book.filter((data)=>data.category==="Free"))
  console.log(res.data);
